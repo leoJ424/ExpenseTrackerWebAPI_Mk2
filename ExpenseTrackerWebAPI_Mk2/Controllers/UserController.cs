b2507a1cefac64c2fe767079e318b8d1b6f5591a
joel <joelmani@gmail.com>
2024-08-10 20:43:34 +0530

    CreateAPI for users

---
+++ ExpenseTrackerWebAPI_Mk2/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using ExpenseTrackerWebAPI_Mk2.Dto;
+using ExpenseTrackerWebAPI_Mk2.Interfaces;
+using ExpenseTrackerWebAPI_Mk2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTrackerWebAPI_Mk2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserController(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateUser([FromBody] UserDto userCreate)
+        {
+            if (userCreate == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = _userRepository.GetAllUsers()
+                                      .Where(u => u.UserName.Trim().ToUpper() == userCreate.UserName.ToUpper()).FirstOrDefault();
+
+            if (user != null)
+            {
+                ModelState.AddModelError("", "User already exists");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userMap = _mapper.Map<User>(userCreate);
+            userMap.Status = true; //Set true by default. Otherwise false is set as default when object is created.
+
+            if (!_userRepository.CreateUser(userMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving.");
+                return StatusCode(500, ModelState);
+            }
+            return Ok("Successfully Created");
+        }
+    }
+}
